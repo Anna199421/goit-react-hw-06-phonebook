c66669dd5f911bcb5e56cd164dc0f51843121d9d
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-04-30 06:44:39 +0300

    gggg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { ContactForm, ContactList, Filter } from './phoneBook';
+import { Toaster } from 'react-hot-toast';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <section>
+        <div className="container">
+          <h1>Phonebook</h1>
+          <ContactForm />
+          <h2>Contacts</h2>
+          <Filter />
+          <ContactList />
+          <Toaster />
+        </div>
+      </section>
     </div>
   );
 };
---
+++ src/components/phoneBook/contactList/ContactList.jsx
@@ -0,0 +1,37 @@
+import { useCallback } from 'react';
+import { Contact } from './Contact';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContacts } from 'redux/index';
+import { filterContacts } from 'components/services/index';
+import css from './ContactList.module.css';
+
+const ContactList = () => {
+  const { contacts, filter } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const filtredContacts = useCallback(
+    () => filterContacts(contacts, filter),
+    [contacts, filter]
+  );
+
+  return (
+    <ul>
+      {filtredContacts().map(({ id, name, number }) => {
+        return (
+          <li className={css.item} key={id}>
+            <Contact name={name} number={number} />
+            <button
+              className={css.button}
+              type="button"
+              onClick={() => dispatch(deleteContacts(id))}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export { ContactList };
